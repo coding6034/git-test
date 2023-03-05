2f365615bf59f399682d9f1028a050ff75bd5dfe
coding <coding@guigu.com>
2023-03-05 11:45:09 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+public class GitTest {
+    public static void main(String args) {
+        System.out.println("hello git-1");
+        System.out.println("hello git-2");
+        System.out.println("hello git-3");
+        System.out.println("hello git-4");
+    }
+}
